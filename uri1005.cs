2802eb379606ab39d94e4c9bb7559c060f1dafa5
Cesar Augusto <130939612+cesar-augusto33@users.noreply.github.com>
2023-09-17 20:03:32 -0300

    Add files via upload

---
+++ uri1005.cs
@@ -0,0 +1,16 @@
+using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double media = (A * 3.5 + B * 7.5)/(3.5 + 7.5);
+
+      Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+
+    }
+
+}
