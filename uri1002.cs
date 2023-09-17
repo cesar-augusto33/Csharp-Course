47504c2821e3542ec38980a1f6b809fd39c553b2
Cesar Augusto <130939612+cesar-augusto33@users.noreply.github.com>
2023-09-17 20:04:06 -0300

    Update uri1002.cs

--- uri1002.cs
+++ uri1002.cs
@@ -8,9 +8,7 @@ namespace Exbeecrowd {
       double n = 3.14159;
       double R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
       double A = n * Math.Pow(R, 2);
-      Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));     
-    
-        
+      Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));            
     }
   }
 }
