f43e2ae437deb8e9cae86547aae4e6008bb9a7b8
Cesar Augusto <130939612+cesar-augusto33@users.noreply.github.com>
2023-09-17 19:57:08 -0300

    Add files via upload

---
+++ uri1003.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      int A = int.Parse(Console.ReadLine());
+      int B = int.Parse(Console.ReadLine());
+      int sum = A + B;
+
+      Console.WriteLine("SOMA = " + sum);
+
+    }
+
+}
