6e002e788a47fb9aeec619e4c2e67a51486a2ed1
Cesar Augusto <130939612+cesar-augusto33@users.noreply.github.com>
2023-09-17 20:02:53 -0300

    Add files via upload

---
+++ uri1002.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+
+namespace Exbeecrowd {
+  class ExBee {
+    static void Main(string[] args) {
+        
+      double n = 3.14159;
+      double R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double A = n * Math.Pow(R, 2);
+      Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));     
+    
+        
+    }
+  }
+}
