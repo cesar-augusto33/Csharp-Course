f97cead18bde07532db8a4566179648fd550fb19
Cesar Augusto <130939612+cesar-augusto33@users.noreply.github.com>
2023-09-19 14:15:23 -0300

    Create uri1016.cs

---
+++ uri1016.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+      double dist = double.Parse(Console.ReadLine());
+
+      double time = dist*2;
+
+      Console.WriteLine(time + " minutos");
+     
+    }
+
+}
